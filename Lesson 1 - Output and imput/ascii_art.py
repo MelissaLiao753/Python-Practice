91d384625de97a679c967f9c7f4091d9d76b6c55
MelissaLiao <liaom@wsc.school.nz>
2026-05-05 14:34:10 +1200

    Lesson 1 - ASCII_art
    Created art of a cat using symbols and the print function
    print("")

---
+++ Lesson 1 - Output and imput/ascii_art.py
@@ -0,0 +1,13 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+
+print ("   ^     ^")
+print (" = o  .  o =")
+print ("<           >   ")
+print ("   --------  ")
+print ("   O      O")
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
