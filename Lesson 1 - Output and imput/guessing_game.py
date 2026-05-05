be6e3b6eb4057d09713a6f5dfb8922767f910226
MelissaLiao <liaom@wsc.school.nz>
2026-05-05 14:45:39 +1200

    Lesson 1 - guessing_game
    Created a guessing game using inputs and print functions.
    
    input("")

---
+++ Lesson 1 - Output and imput/guessing_game.py
@@ -0,0 +1,26 @@
+### Create a 3-part guessing game ###
+input("Hello! Whats a nephron?")
+# Give your user the first hint and wait for input
+input("Not quite. It's related to the body. Try again!")
+# Give your user the second hint and wait for input
+input("Here's another hint. It's in the kidneys.")
+# Give your user the final hint and wait for input
+input("So close. It has to do with removing waste.")
+# Tell your user the answer
+print("You almost had it. A nephron is a small microscopic unit in the kidneys that are crucial to filtering blood, and removing waste. These are key parts to the kidney!")
+
+
+# ------------------------------
+
+# EXTENSION
+# Create another guessing game
+
+
+# ------------------------------
+
+# EXPERT (for those who already know some Python)
+# Your 3-part guessing game should have:
+# + An introduction
+# + A conclusion
+# + It should check if the user is correct and stop giving hints if they are
+# + It should give points based on how quickly the user got it corre
