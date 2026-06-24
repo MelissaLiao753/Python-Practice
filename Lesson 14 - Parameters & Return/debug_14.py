12f45a5bd0b6eae9d4ec06b07d921a50f1340879
MelissaLiao <liaom@wsc.school.nz>
2026-06-25 10:23:32 +1200

    Fixing bugs with parameters and functions

---
+++ Lesson 14 - Parameters & Return/debug_14.py
@@ -0,0 +1,17 @@
+# DEFINING AMOUNT OF CALORIES BURNT
+def burn_calories(current_total, activity_mins):
+    calories_burned = activity_mins * 8.5
+    new_total = current_total - calories_burned
+    return new_total
+#DEFINING THE AMOUNT OF CALORIES CONSUMED (WITH MEAL)
+def log_meal(base_calories, meal_weight):
+    total_calories = base_calories + meal_weight
+    return total_calories
+
+def main():
+    daily_calories = 2000.0
+    log_meal(daily_calories, 450.0)
+    print(f"Calories after breakfast: {daily_calories}")
+    remaining_calories = burn_calories(daily_calories, 30)
+    print(f"Remaining calories after workout: {remaining_calories}")
+main()
